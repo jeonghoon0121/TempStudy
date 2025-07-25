a7cd92138c07db3d6d9f851968b12d403cfdcc01
JHPC003\hnjh1 <hnjh12195@naver.com>
2025-07-25 17:36:12 +0900

    1

--- P01_practice/practice250725/src/main/java/com/good/Application.java
+++ P01_practice/practice250725/src/main/java/com/good/Application.java
@@ -6,13 +6,13 @@ public class Application {
         animal1.cry();
         animal1.eat();
         animal1.run();
+
         Animal animal2=new fish();
         animal2.cry();
         animal2.run();
         animal2.eat();
-        Animal A;
-        A=(animal1);
-        A.cry();
+        //up캐스팅
+
 
     }
 }
